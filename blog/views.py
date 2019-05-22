c45386e38824cfe10479bab63c173a987acab5df
Julio Raygoza <monk16biit@gmail.com>
2019-05-21 18:57:44 -0700

    Profile

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
+posts = [
+
+    {
+        'author': 'monk',
+        'title': 'Blog post 1',
+        'content': 'My first blog post.',
+        'date_posted': 'May 18, 2019'
+    },
+    {
+        'author': 'Joy',
+        'title': 'Got milk?',
+        'content': 'I want milk!',
+        'date_posted': 'May 19, 2019'
+    }
+
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Blogs-Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'Django-Blog'})
